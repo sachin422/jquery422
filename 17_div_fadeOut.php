0ad88b6bccfae3d586070237d708dc3508afbafa
sachin jasrotia <sachin422@gmail.com>
2017-03-17 15:03:43 +0530

    Add files via upload
    select all div apply fadeTo (disappear) action first param : (duration)

---
+++ 17_div_fadeOut.php
@@ -0,0 +1,23 @@
+<html lang="en">
+	<head>
+		<head>
+			<title>My First jquery file</title>
+			<script src="jquery-3.2.0.js"></script>
+			<script>
+				$(function(){
+					
+					$("div").fadeOut(4000);
+					
+					// select all div apply fadeTo (disappear) action first param : (duration)
+					
+				});
+			</script>
+		</head>
+		<body>
+			<div>first</div>	
+			<div>second</div>	
+			<div>third</div>	
+			<div>fourth</div>	
+		</body>
+	</head>
+</html>
