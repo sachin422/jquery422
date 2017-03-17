9bbd87f243733c43655cca289ee977ac78bf6fe1
sachin jasrotia <sachin422@gmail.com>
2017-03-17 13:28:03 +0530

    Add files via upload
    my first program demo

---
+++ 1_initialize_jquery_environment.php
@@ -0,0 +1,18 @@
+<html lang="en">
+	<head>
+		<head>
+			<title>My First jquery file</title>
+			<script src="jquery-3.2.0.js"></script>
+			<script>
+				$(function(){
+					
+					alert('my first jquery program');	
+					
+				});
+			</script>
+		</head>
+		<body>
+					
+		</body>
+	</head>
+</html>
