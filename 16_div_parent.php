2d7c7e682162a31b9ec27d134c11d29d772ecc9f
sachin jasrotia <sachin422@gmail.com>
2017-03-17 15:00:39 +0530

    Add files via upload
    select all div parent apply css border style

---
+++ 16_div_parent.php
@@ -0,0 +1,20 @@
+<html lang="en">
+	<head>
+		<head>
+			<title>My First jquery file</title>
+			<script src="jquery-3.2.0.js"></script>
+			<script>
+				$(function(){
+					
+					$("div:parent").css("border","1px solid red");
+					
+					// select all div parent apply css border style
+					
+				});
+			</script>
+		</head>
+		<body>
+			<h1>h1 tag <h5> h5 tag<div>first</div></h5></h1>
+		</body>
+	</head>
+</html>
