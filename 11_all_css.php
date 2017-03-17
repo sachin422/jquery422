ade77cb1b3e6d0ae76c7090d7ef6a904ebece34f
sachin jasrotia <sachin422@gmail.com>
2017-03-17 14:41:23 +0530

    Add files via upload
    select all tag from body and apply css border style

---
+++ 11_all_css.php
@@ -0,0 +1,23 @@
+<html lang="en">
+	<head>
+		<head>
+			<title>My First jquery file</title>
+			<script src="jquery-3.2.0.js"></script>
+			<script>
+				$(function(){
+					
+					$("*").css("border","1px solid red");
+					
+					// select all tag from body and apply css border style
+					
+				});
+			</script>
+		</head>
+		<body>
+			<div>zero</div>	
+			<div>first</div>	
+			<div>second</div>	
+			<div>third</div>	
+		</body>
+	</head>
+</html>
