9bf33b1e98aac63d98e306435a3c4cda614c4ce2
sachin jasrotia <sachin422@gmail.com>
2017-03-17 13:37:10 +0530

    Add files via upload
    select all div put string sachin422 after all div

---
+++ 4_div_after.php
@@ -0,0 +1,23 @@
+<html lang="en">
+	<head>
+		<head>
+			<title>My First jquery file</title>
+			<script src="jquery-3.2.0.js"></script>
+			<script>
+				$(function(){
+					
+					$("div").after("sachin422");
+					
+					// select all div put string sachin422 after all div
+					
+				});
+			</script>
+		</head>
+		<body>
+				<div>first</div>	
+				<div>second</div>	
+				<div>third</div>	
+				<div>fourth</div>	
+		</body>
+	</head>
+</html>
