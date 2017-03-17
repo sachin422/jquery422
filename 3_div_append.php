728834ac2ffdbcc5e3e740c3dc65ab078a99ebf7
sachin jasrotia <sachin422@gmail.com>
2017-03-17 13:35:00 +0530

    Add files via upload
    select all div and append with text sachin422

---
+++ 3_div_append.php
@@ -0,0 +1,23 @@
+<html lang="en">
+	<head>
+		<head>
+			<title>My First jquery file</title>
+			<script src="jquery-3.2.0.js"></script>
+			<script>
+				$(function(){
+					
+					$("div").append("sachin422");
+					
+					// select all div and append with text sachin422
+					
+				});
+			</script>
+		</head>
+		<body>
+				<div>first</div>	
+				<div>second</div>	
+				<div>third</div>	
+				<div>fourth</div>	
+		</body>
+	</head>
+</html>
