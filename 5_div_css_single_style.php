7b80867714d2cf255b32fc565450eb0429a319e1
sachin jasrotia <sachin422@gmail.com>
2017-03-17 13:44:58 +0530

    Add files via upload
    select all div put border 1px solid red around all divs with single css style

---
+++ 5_div_css_single_style.php
@@ -0,0 +1,23 @@
+<html lang="en">
+	<head>
+		<head>
+			<title>My First jquery file</title>
+			<script src="jquery-3.2.0.js"></script>
+			<script>
+				$(function(){
+					
+					$("div").css("border","1px solid red");
+					
+					// select all div put border 1px solid red around all divs with single css style
+					
+				});
+			</script>
+		</head>
+		<body>
+				<div>first</div>	
+				<div>second</div>	
+				<div>third</div>	
+				<div>fourth</div>	
+		</body>
+	</head>
+</html>
