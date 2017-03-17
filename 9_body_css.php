27260177f63faa1e981aaf36fecaa9573e8fd48b
sachin jasrotia <sachin422@gmail.com>
2017-03-17 14:36:24 +0530

    Add files via upload
    apply css background color to doucment.body

---
+++ 9_body_css.php
@@ -0,0 +1,20 @@
+<html lang="en">
+	<head>
+		<head>
+			<title>My First jquery file</title>
+			<script src="jquery-3.2.0.js"></script>
+			<script>
+				$(function(){
+					
+					$(document.body).css("background-color","#f00");
+					
+					// apply css background color to doucment.body
+					
+				});
+			</script>
+		</head>
+		<body>
+				
+		</body>
+	</head>
+</html>
