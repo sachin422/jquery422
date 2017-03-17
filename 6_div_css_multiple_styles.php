4efebab984cdb474c17cb9b72e48486004600361
sachin jasrotia <sachin422@gmail.com>
2017-03-17 13:47:50 +0530

    Add files via upload
    select all div and apply multiple styles to all div. multiple css style must be passed in json format

---
+++ 6_div_css_multiple_styles.php
@@ -0,0 +1,23 @@
+<html lang="en">
+	<head>
+		<head>
+			<title>My First jquery file</title>
+			<script src="jquery-3.2.0.js"></script>
+			<script>
+				$(function(){
+					
+					$("div").css({"border":"1px solid red","background-color":"#00ff00"});
+					
+					// select all div and apply multiple styles to all div. multiple css style must be passed in json format
+					
+				});
+			</script>
+		</head>
+		<body>
+				<div>first</div>	
+				<div>second</div>	
+				<div>third</div>	
+				<div>fourth</div>	
+		</body>
+	</head>
+</html>
