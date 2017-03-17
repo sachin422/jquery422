f25149d48b2fd378a6558568d5e646334d001e1e
sachin jasrotia <sachin422@gmail.com>
2017-03-17 13:31:30 +0530

    Add files via upload
    select all div from dom and populate with custom text

---
+++ 2_div_html.php
@@ -0,0 +1,23 @@
+<html lang="en">
+	<head>
+		<head>
+			<title>My First jquery file</title>
+			<script src="jquery-3.2.0.js"></script>
+			<script>
+				$(function(){
+					
+					$("div").html("sachin422");
+					
+					// select all div and populate with text sachin422
+					
+				});
+			</script>
+		</head>
+		<body>
+				<div></div>	
+				<div></div>	
+				<div></div>	
+				<div></div>	
+		</body>
+	</head>
+</html>
