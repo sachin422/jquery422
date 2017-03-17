e767802f53dee1bc747b9e0187aea442df01e015
sachin jasrotia <sachin422@gmail.com>
2017-03-17 16:27:16 +0530

    Add files via upload
    select input who is enabled and set its value to "sachin422"

---
+++ 21_input_enabled.php
@@ -0,0 +1,23 @@
+<html lang="en">
+	<head>
+		<head>
+			<title>My First jquery file</title>
+			<script src="jquery-3.2.0.js"></script>
+			<script>
+				$(function(){
+					
+					$("input:enabled").val("sachin422");
+					
+					// select input who is enabled and set its value to "sachin422"
+					
+				});
+			</script>
+		</head>
+		<body>
+			
+				<input />
+				<input disabled="disabled" />
+			
+		</body>
+	</head>
+</html>
