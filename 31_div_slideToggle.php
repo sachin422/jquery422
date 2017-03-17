5da082912e873d41f858f66c2d9b2dd743a3cfa1
sachin jasrotia <sachin422@gmail.com>
2017-03-17 17:28:58 +0530

    Add files via upload
    select all div elements and toggle them (hide with animation)

---
+++ 31_div_slideToggle.php
@@ -0,0 +1,27 @@
+<html lang="en">
+	<head>
+		<head>
+			<title>My First jquery file</title>
+			<script src="jquery-3.2.0.js"></script>
+			<script>
+				$(function(){
+					
+					$("div").slideToggle();
+					
+					// select all div elements and hide them
+					
+					
+				});
+			</script>
+		</head>
+		<body>
+				
+				<div>first</div>
+				<div>second</div>
+				<div>third</div>
+				<div>fourth</div>
+				
+			
+		</body>
+	</head>
+</html>
