c5b66a20dc8ab736e4c8004efdf828518b32e97d
sachin jasrotia <sachin422@gmail.com>
2017-03-17 14:54:02 +0530

    Add files via upload
    select all div apply fadeTo action first param : (duration) and second param : (opacity)

---
+++ 15_div_fadeTo.php
@@ -0,0 +1,23 @@
+<html lang="en">
+	<head>
+		<head>
+			<title>My First jquery file</title>
+			<script src="jquery-3.2.0.js"></script>
+			<script>
+				$(function(){
+					
+					$("div").fadeTo("slow",0.25);
+					
+					// select all div apply fadeTo action first param : (duration) and second param : (opacity)
+					
+				});
+			</script>
+		</head>
+		<body>
+			<div>first</div>	
+			<div>second</div>	
+			<div>third</div>	
+			<div>fourth</div>	
+		</body>
+	</head>
+</html>
